4dbf875d1041b1f099577dc414c36ccf47b9e6e5
Knight of belief <slbdenis.victor@gmail.com>
2023-09-28 14:52:27 +0300

    added goose statement

---
+++ internal/storage/migration/20230928141652_create_articles.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE articles (
+    id SERIAL PRIMARY KEY,
+    source_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    summary TEXT NOT NULL,
+    published_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    posted_at TIMESTAMP,
+    CONSTRAINT fk_articles_source_id
+        FOREIGN KEY(source_id)
+            REFERENCES sources (id)
+            ON DELETE CASCADE
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS articles;
+-- +goose StatementEnd

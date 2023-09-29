e94e3b0533792c1beb739da86d3aac600c7c9cc0
Knight of belief <slbdenis.victor@gmail.com>
2023-09-29 13:11:28 +0300

    debug table articles

--- internal/storage/migration/20230928141652_create_articles.sql
+++ internal/storage/migration/20230928141652_create_articles.sql
@@ -4,10 +4,10 @@ CREATE TABLE articles (
     id SERIAL PRIMARY KEY,
     source_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
-    link VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL UNIQUE,
     summary TEXT NOT NULL,
     published_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     posted_at TIMESTAMP,
     CONSTRAINT fk_articles_source_id
         FOREIGN KEY(source_id)

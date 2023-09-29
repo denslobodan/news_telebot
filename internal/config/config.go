9487059984087ff38728a4f23916d13ab5b7fd7c
Knight of belief <slbdenis.victor@gmail.com>
2023-09-29 13:58:45 +0300

    Added config

---
+++ internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/cristalhq/aconfig"
+	"github.com/cristalhq/aconfig/aconfighcl"
+)
+
+type Config struct {
+	TelegramBotToken     string        `hcl:"telegram_bot_tiken" env:"TELEGRAM_BOT_TOKEN" required:"true"`
+	TelegramChannelID    int64         `hcl:"telegram_channel_id" env:"TELEGRAM_CHANNEL_ID" required:"true"`
+	DatabaseDSN          string        `hcl:"database_dsn" env:"DATABASE_DSN" default:"postgres://postgres:postgres@localhost:5432/news_feed_bot?sslmodel=disable"`
+	FetchInterval        time.Duration `hcl:"fetch_interval" env:"FETCH_INTERVAL" default:"10m"`
+	NotificationInterval time.Duration `hcl:"notification_interval" env:"NOTIFICATION_INTERVAL" default:"1m"`
+	FilterKeywords       []string      `hcl:"filter_keywords" env:"FILTER_KEYWORDS"`
+	OpenAIKey            string        `hcl:"openai_key" env:"OPENAI_KEY"`
+	OpenAIPromt          string        `hcl:"openai_promt" env:"OPENAI_PROMT"`
+}
+
+var (
+	cfg  Config
+	once sync.Once
+)
+
+func Get() Config {
+	once.Do(func() {
+		loader := aconfig.LoaderFor(&cfg, aconfig.Config{
+			EnvPrefix: "NFB",
+			Files:     []string{"./config.hcl", "./config.local.hcl"},
+			FileDecoders: map[string]aconfig.FileDecoder{
+				".hcl": aconfighcl.New(),
+			},
+		})
+
+		if err := loader.Load(); err != nil {
+			log.Printf("[ERROR] failed to load config: %v", err)
+		}
+	})
+
+	return cfg
+}
